28a2701f23e1e13298ec43e1044af64b9a7d135f
HAK.KIM <HAK.KIM@PNC.LAN>
2022-01-26 11:32:19 +0700

    #6

--- JavaScript/App.js
+++ JavaScript/App.js
@@ -1,53 +1,3 @@
-
-// hide banner and show start quiz
-
-function displayStartQuze(){
-    banner.style.display='none'
-    createQuiz.style.display='none'
-    quiz.style.display='none'
-    start_quiz.style.display='block'
-    
-
-}
-let start_quiz=document.getElementById('start_quiz')
-let banner=document.getElementById('banner')
-
-let clickOnMenuQuiz=document.getElementById('quize-bar')
-clickOnMenuQuiz.addEventListener('click', displayStartQuze)
-
-// hide shart quiz and show play quiz
-
-function playQuiz(){
-    start_quiz.style.display='none'
-    createQuiz.style.display='none'
-    quiz.style.display='block'
-   
-
-}
-// let quiz=document.getElementById('quiz')
-let clickOnBtnStartQuiz=document.getElementById('btn-start')
-clickOnBtnStartQuiz.addEventListener('click', playQuiz)
-
-// click on btn create to show create-form and hide all
-function displayCreateForm(){
-    start_quiz.style.display='none'
-    quiz.style.display='none'
-    banner.style.display='none'
-    createQuiz.style.display='block'
-
-
-}
-let createQuiz=document.getElementById('createForm')
-let clickOnBtnCreate=document.getElementById('create-bar')
-clickOnBtnCreate.addEventListener('click', displayCreateForm)
-
-
-
-
-
-
-// ulvy responsible
-
 // get value from from-container
 var container = document.querySelector(".form-container");
 container.style.display = "none";
@@ -57,8 +7,6 @@ var showQue = document.querySelector(".show-que");
 
 var formGroup = document.querySelector(".form-group");
 
-
-var form_Group = document.querySelector(".form-group");
 
 function create_display(event) {
     event.preventDefault();
@@ -80,103 +28,66 @@ function create_display(event) {
     showQuestion.className = "show-que";
     
     let div_P = document.createElement("div");
-    let P_div = document.createElement("p");
-    P_div.textContent = 'get_Q';
+    let P_div = document.createElement("span");
+    P_div.textContent = get_Q;
     P_div.className = "q_tion";
     div_P.appendChild(P_div);
 
     showQuestion.appendChild(div_P);
 
     let div_An1 = document.createElement("div");
-    let input_an1 = document.createElement("input");
-    input_an1.className='rad'
-    input_an1.type = "radio";
-    input_an1.id = "rad";
-    let label_An1 = document.createElement("label");
-    label_An1.className = "radio";
-    label_An1.textContent = 'getA1';
+    let input_an1 = document.createElement("span");
+    input_an1.type = "text";
+    input_an1.className = "rad1";
+    input_an1.textContent ="Answer 1 : "+ getA1;
     div_An1.appendChild(input_an1)
-    div_An1.appendChild(label_An1)
     showQuestion.appendChild(div_An1);
 
 
     let div_An2 = document.createElement("div");
-    let input_an2 = document.createElement("input");
-    input_an2.type = "radio"
-    input_an2.id = "rad1";
-    input_an2.className='rad'
-    let label_An2 = document.createElement("label");
-    label_An2.className = "radio";
-    label_An2.textContent = 'getA2';
+    let input_an2 = document.createElement("span");
+    input_an2.type = "text"
+    input_an2.className = "rad1";
+    input_an2.textContent ="Answer 2 : "+ getA2;
     div_An2.appendChild(input_an2)
-    div_An2.appendChild(label_An2)
     showQuestion.appendChild(div_An2);
 
     let div_An3 = document.createElement("div");
-    let input_an3 = document.createElement("input");
-    input_an3.type = "radio";
-    input_an3.id = "rad2";
-    input_an3.className='rad'
-    let label_An3 = document.createElement("label");
-    label_An3.className = "radio";
-    label_An3.textContent = 'getA3';
+    let input_an3 = document.createElement("span");
+    input_an3.type = "text";
+    input_an3.className = "rad1";
+    input_an3.textContent = "Answer 3 : "+getA3;
     div_An3.appendChild(input_an3)
-    div_An3.appendChild(label_An3)
     showQuestion.appendChild(div_An3);
 
     
     let div_An4 = document.createElement("div");
-    let input_an4 = document.createElement("input");
-    input_an4.type = "radio";
-    input_an4.id = "rad3";
-    let label_An4 = document.createElement("label");
-    label_An4.className = "radio";
-    label_An4.textContent = 'getA4';
+    let input_an4 = document.createElement("span");
+    input_an4.type = "text";
+    input_an4.className = "rad1";
+    input_an4.textContent = "Answer 4 : "+getA4;
     div_An4.appendChild(input_an4)
-    div_An4.appendChild(label_An4)
     showQuestion.appendChild(div_An4);
 
-  
-    document.body.appendChild(showQuestion);
-    console.log(arrayOfDict);
+    let div_correct = document.createElement("div");
+    let corr = document.createElement("span");
+    corr.textContent ="Correct Answer : "+ correctAnswer;
+    corr.className = "rad1"
+    div_correct.appendChild(corr);
+    showQuestion.appendChild(div_correct);
+    
+    container.appendChild(showQuestion);
 
-    let get_valueLabel = document.querySelectorAll(".show-que div label");
-    let IfLooped = false;
-    for ( let index of arrayOfDict){
-
-        for (let val in index){
-
-            if (val == "correct" && !IfLooped){
-                console.log(index[val]);
-                console.log(get_valueLabel[0].textContent );
-                if ( get_valueLabel[0].textContent == index[val] ){
-                    document.getElementById("rad").checked= true;
-                }
-
-                else if ( get_valueLabel[1].textContent == index[val] ){
-                    document.getElementById("rad1").checked= true;
-
-                }
-
-                else if ( get_valueLabel[2].textContent == index[val] ){
-                    document.getElementById("rad2").checked= true;
-
-                }
-
-                else if ( get_valueLabel[3].textContent == index[val] ){
-                    document.getElementById("rad3").checked= true;
-                    console.log("hfds");
-
-                }
-                
-                IfLooped = true;
-            }
-        }
-    }
+    // clear all question after click on btn add
+    var getA1 = document.getElementById("a1").value = "";
+    var getA2 = document.getElementById("a2").value = "";
+    var getA3 = document.getElementById("a3").value = "";
+    var getA4 = document.getElementById("a4").value = "";
+    var get_Q = document.getElementById("Que").value = "";
+    var correctAnswer = document.getElementById("correct_Ans").value = "";
     
 
 }
-
 // show quiz form for create 
 function show_quiz(event) {
 
@@ -184,17 +95,6 @@ function show_quiz(event) {
     container.style.display = "block";
 }
 
-// clear question and answer
-function clearQuesAns (event) {
-    event.preventDefault();
-    var getA1 = document.getElementById("a1").value = "";
-    var getA2 = document.getElementById("a2").value = "";
-    var getA3 = document.getElementById("a3").value = "";
-    var getA4 = document.getElementById("a4").value = "";
-    var get_Q = document.getElementById("Que").value = "";
-    var correctAnswer = document.getElementById("correct_Ans").value = "";
-
-}
 
 // get value from question
 
@@ -219,6 +119,4 @@ getCreateValue.addEventListener("click", show_quiz);
 // btn-add
 let btn_next = document.querySelector(".next");
 btn_next.addEventListener("click",create_display)
-
-
 
