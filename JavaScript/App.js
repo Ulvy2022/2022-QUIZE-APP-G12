f8832b915e34f59dda17b90a3ed60ca6de930784
HAK.KIM <HAK.KIM@PNC.LAN>
2022-01-26 11:35:24 +0700

    #18 hide show

--- JavaScript/App.js
+++ JavaScript/App.js
@@ -1,3 +1,57 @@
+
+
+// hide banner and show start quiz
+function displayStartQuze(){
+    banner.style.display='none'
+    createQuiz.style.display='none'
+    // formShowQuestion.style.display='none'
+    quiz.style.display='none'
+    start_quiz.style.display='block'
+    
+}
+let start_quiz=document.getElementById('start_quiz')
+let banner=document.getElementById('banner')
+
+let clickOnMenuQuiz=document.getElementById('quize-bar')
+clickOnMenuQuiz.addEventListener('click', displayStartQuze)
+
+
+
+// hide shart quiz and show play quiz
+function playQuiz(){
+    start_quiz.style.display='none'
+    // formShowQuestion.style.display='none'
+    createQuiz.style.display='none'
+    quiz.style.display='block'
+
+}
+
+let clickOnBtnStartQuiz=document.getElementById('btn-start')
+clickOnBtnStartQuiz.addEventListener('click', playQuiz)
+
+
+
+// click on btn create to show create-form and hide all
+function displayCreateForm(){
+    // formShowQuestion.style.display='none'
+    start_quiz.style.display='none'
+    quiz.style.display='none'
+    banner.style.display='none'
+    createQuiz.style.display='block'
+
+}
+let createQuiz=document.getElementById('createForm')
+let clickOnBtnCreate=document.getElementById('create-bar')
+clickOnBtnCreate.addEventListener('click', displayCreateForm)
+
+
+
+
+
+
+
+
+
 // get value from from-container
 var container = document.querySelector(".form-container");
 container.style.display = "none";
